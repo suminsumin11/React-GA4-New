256b1aa75be18d7af728f9473461224d7b7e237f
suminsumin11 <leess21088@naver.com>
2025-05-08 20:17:18 +0900

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
+
+const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag &&
+      window.gtag("event", "page_view", {
+        page_path: location.pathname + location.search,
+      });
+  }, [location]);
+};
 
 function App() {
   const [count, setCount] = useState(0);
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  usePageTracking();
   return (
     <div className="App">
       <h1>Simple Counter</h1>
