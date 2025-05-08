039d5fda0027bf8197c2f45ea73c6ce757067b3f
suminsumin11 <leess21088@naver.com>
2025-05-08 19:34:10 +0900

    first

---
+++ src/components/Viewer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Viewer.css";
+
+const Viewer = ({ count }) => {
+  return (
+    <div className="Viewer">
+      <div>현재 카운트:</div>
+      <h1>{count}</h1>
+    </div>
+  );
+};
+
+export default Viewer;
